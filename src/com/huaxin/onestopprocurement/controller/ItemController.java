27ab1a0ffec0c65a670a899bc220489b89d4ac58
Administrator <Administrator@192.168.0.100>
2019-09-28 18:12:56 +0800

    refine

--- src/com/huaxin/onestopprocurement/controller/ItemController.java
+++ src/com/huaxin/onestopprocurement/controller/ItemController.java
@@ -2,27 +2,17 @@
 package com.huaxin.onestopprocurement.controller;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.huaxin.onestopprocurement.po.CartCustom;
-import com.huaxin.onestopprocurement.po.Item;
 import com.huaxin.onestopprocurement.po.ItemCustom;
 import com.huaxin.onestopprocurement.po.ItemQueryVo;
 import com.huaxin.onestopprocurement.po.Itemclasses;
